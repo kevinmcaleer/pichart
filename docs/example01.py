08d9faf64243dfcf833aac5c63b3111b443cad4e
Kevin McAleer <kevinmcaleer@gmail.com>
2025-02-22 10:04:08 +0000

    updated secrets

---
+++ docs/example01.py
@@ -0,0 +1,13 @@
+from pichart import Chart
+from presto import Presto
+
+presto = Presto(ambient_light=True)
+display = presto.display
+WIDTH, HEIGHT = display.get_bounds()
+
+chart = Chart(display, title="Temp", x_values=[20, 22, 25, 23])
+chart.width = 240
+chart.height = 135
+chart.data_colour = {'red': 255, 'green': 0, 'blue': 0}
+chart.update()
+presto.update()
