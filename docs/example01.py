2f7355628dcc505a791ac1972aabd9ba3e5befb2
Kevin McAleer <kevinmcaleer@gmail.com>
2025-02-22 18:50:25 +0000

    Version 2.2.0

--- docs/example01.py
+++ docs/example01.py
@@ -5,7 +5,7 @@ presto = Presto(ambient_light=True)
 display = presto.display
 WIDTH, HEIGHT = display.get_bounds()
 
-chart = Chart(display, title="Temp", x_values=[20, 22, 25, 23])
+chart = Chart(display, title="Temp", values=[20, 22, 25, 23, 34])
 chart.width = 240
 chart.height = 135
 chart.data_colour = {'red': 255, 'green': 0, 'blue': 0}
